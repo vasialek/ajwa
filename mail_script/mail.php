d8c93569d18c3319a15aa64ff8d3cabd11c9a87c
StaffNowa <vasilij@prado.lt>
2013-05-07 22:52:11 +0300

    * fixed problem with getEmails() return array of mail data

--- mail_script/mail.php
+++ mail_script/mail.php
@@ -11,8 +11,8 @@ class AiMail
 {
     private $_mailHost = "smtp.gmail.com";
     private $_mailProtocol = "tls";
-    private $_mailUsername = "test@prado.lt";
-    private $_mailPassword = "D9&4VPgy";
+    private $_mailUsername = "username@gmail.com";
+    private $_mailPassword = "password";
     private $_mailPort = 995;
 
     public function getMailHost()
@@ -40,14 +40,20 @@ class AiMail
         return $this->_mailPort;
     }
 
-    function getEmails($darid = '0', $adrid = '0', $uzid = '0', $box = '0', $mail = '0', $rodyti = '0')
+    /**
+     * Get email from server by POP3 protocol
+     *
+     * @return array
+     * @throw POP3_Exception
+     */
+
+    function getEmails()
     {
         $objMails = new mail_vaizdas();
 
         /**
          * Remember that the encryption support doesn't work at time for the socket extension
          * This will I implement later.
-         *
          */
         $bUseSockets = FALSE;
         $bUseTLS = TRUE;
@@ -66,7 +72,6 @@ class AiMail
         $strPathToDir = $strRootPath . "mails" . DIRECTORY_SEPARATOR;
         $strFileEndings = ".eml";
 
-
         try {
             // Instance the POP3 object
             $objPOP3 = new POP3($strLogFile, $bAPopAutoDetect, $bHideUsernameAtLog, $this->getMailProtocol(), $bUseSockets);
@@ -79,27 +84,20 @@ class AiMail
 
             // Get the office status
             $arrOfficeStatus = $objPOP3->getOfficeStatus();
+            $mailArr = array();
+            $i = 0;
 
-            /**
-             * This for loop store the messages under their message number on the server
-             * and mark the message as delete on the server.
-             */
-            //"Hello world!";
-            for ($intMsgNum = 1; $intMsgNum <= $arrOfficeStatus["count"]; $intMsgNum++) {
-                $EML1 = $objPOP3->getMsg($intMsgNum);
+            for($intMsgNum = 1; $intMsgNum <= $arrOfficeStatus["count"]; $intMsgNum++ )
+            {
+                $EML1=$objPOP3->getMsg($intMsgNum);
 
                 $mail_obj = new MIMEDECODE($EML1, "\r\n");
                 $mail = $mail_obj->get_parsed_message();
+
+                $mailArr[$i] = $mail;
+//                var_dump($mailArr);
                 $i++;
-
-
-                //$objPOP3->insertToDB($mail);
-                //$objPOP3->saveToFileFromServer($intMsgNum, $strPathToDir, $strFileEndings);
-                //        $objPOP3->deleteMsg($intMsgNum);
-
             }
-
-            //$disp = "<div class='td-thead-l'>Gauta naujų laiškų: <b>{$arrOfficeStatus['count']}</b></div></br>";
 
             // Send the quit command and all as delete marked message will remove from the server.
             // IMPORTANT:
@@ -113,36 +111,13 @@ class AiMail
             //   To delete the mails from the server you have to send the quit command themself before disconnecting from the server
             $objPOP3->disconnect();
 
-            //atvaizduojam laiskus
+            return $mailArr;
 
-            /*if ($mail=='new')
-            {
-                $disp.= $objMails->displayNewMail();
-                if(!empty($_POST['send'])) $disp.=$objMails->sendMail($this->getMailUsername(), $this->_mailPassword, $rez->d_email);
-                if(!empty($_POST['save'])) $disp.=$objMails->saveMail($this->getMailUsername(), $this->getMailPassword(), $rez->d_email);
-                if(!empty($_FILES['file'])) $disp.=$objMails->upload_file();
-            }
-            elseif ($uzid>0)
-            {
-                $visi_laiskai = $objMails->uzid_adrid();
-                $arr = $objMails->uz_mail($visi_laiskai);
-                $disp.= $objMails->displayUzMail($arr);
-            }
-            else
-            {
-                if($box=='siustieji') $arr = $objMails->getMails(null, $this->getMailUsername());
-                elseif($box=='juodrastis') $arr = $objMails->getMails(null,null,'1');
-                else $arr = $objMails->getMails($this->getMailUsername(),null);
-                $disp.= $objMails->displayWebMail($arr);
-            }*/
-
-            //var_dump('testas: ' . $objMails->getMails('test@prado.lt', null));
-
-            return $mail;
         } catch (POP3_Exception $e) {
             die($e);
         }
-        return $disp;
+
+        return $mailArr;
     }
 }
 
@@ -151,14 +126,17 @@ class AiMail
 $mail = new AiMail();
 $mailData = $mail->getEmails();
 //var_dump($mailData);
-for ($i = 0; $i < count($mailData); $i++) {
-    echo "Date: " . $mailData[$i]['Date'] . "<br />";
-    echo "From: " . $mailData[$i]['adresas'] . "<br />";
-    echo "First name: " . $mailData[$i]['vardas'] . "<br />";
-    echo 'To: ' . $mailData[$i]['To'] . "<br />";
-    echo "Subject: " . $mailData[$i]['Subject'] . "<br />";
-    echo "Message (plain): " . $mailData[$i]['Message']['plain'] . "<br />";
-    echo "Message (html): " . $mailData[$i]['Message']['html'] . "<br />";
+
+for ($i = 0; $i < count($mailData); $i++)
+{
+    echo "Date: " . date("Y-m-d H:i:s", strtotime($mailData[$i][0]['Date'])) . "<br />";
+    echo "From: " . $mailData[$i][0]['adresas'] . "<br />";
+    echo "First name: " . $mailData[$i][0]['vardas'] . "<br />";
+    echo 'To: ' . $mailData[$i][0]['To'] . "<br />";
+    echo "Subject: " . $mailData[$i][0]['Subject'] . "<br />";
+    echo "Message (plain): " . $mailData[$i][0]['Message']['plain'] . "<br />";
+    echo "Message (html): " . $mailData[$i][0]['Message']['html'] . "<br /><br />";
+    echo "----------------------------------------<br />";
 }
 
 ?> 
